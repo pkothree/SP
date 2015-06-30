d84c8dab9a3030f090758e929672ef90d300189a
Danny Davis <pko3@gmx.de>
2015-06-30 21:17:38 +0200

    .

--- powershell_commands.ps1
+++ powershell_commands.ps1
@@ -6,3 +6,6 @@ Get-ChildItem Env:
 
 #Get certificates
 Get-ChildItem -Recurse cert:
+
+#Get events from a certain Type
+Get-SPLogEvent | ?{$_.Area -eq "Excel Services Application" -And $_.Category -eq "Data Model" } | Format-List
