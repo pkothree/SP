ead92cf816bca88fe298cf5719ae8cc807a80bc1
Danny Davis <pko3@gmx.de>
2015-07-10 06:05:18 +0200

    Added
    Untested version of this script.

---
+++ ActivateAlternateLanguages.ps1
@@ -0,0 +1,26 @@
+if ( (Get-PSSnapin -Name Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue) -eq $null )
+{
+Add-PSSnapin Microsoft.SharePoint.PowerShell
+}
+
+$webApp = Get-SPWebApplication
+
+foreach($site in $webApp.Sites)
+{
+  $web = $site.Rootweb
+
+  # If you want to automatically activate languages on all Sites
+  #$web.IsMultilingual = $true;
+
+  # Check if Multilanguage Support is activated
+  # If so, activate all the other available languages
+  if($web.IsMultilingual -eq $true)
+  {
+    foreach($culture in $web.SupportedUICultures)
+    {
+      $web.AddSupportedUICulture($culture.LCID)
+      $web.Update()
+    }
+  }
+  $web.Dispose()
+}
