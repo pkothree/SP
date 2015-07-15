ee27440ff272cb018ebef6e77ba2c7be2eadb272
Danny Davis <pko3@gmx.de>
2015-07-15 11:28:03 +0200

    Fixed Bugs

--- ActivateAlternateLanguages.ps1
+++ ActivateAlternateLanguages.ps1
@@ -1,11 +1,10 @@
 ###
 # Danny Davis
-# twitter: twitter.com/pko3
-# github: github.com/pkothree
 # Created: 07/10/15
-# Modified: 07/14/15
+# Modified: 07/15/15
 # Description: Activate all alternate languages on all site collections
 ###
+
 
 if ( (Get-PSSnapin -Name Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue) -eq $null )
 {
@@ -13,26 +12,36 @@ Add-PSSnapin Microsoft.SharePoint.PowerShell
 }
 
 $webApp = Get-SPWebApplication WEBAPP
+$w = $WebApp.Name
+
+start-transcript -path $w".txt"
 
 foreach($site in $webApp.Sites)
 {
   $web = $site.Rootweb
-  write-host -Backgroundcolor Green -Foregroundcolor Black "Web: " $web
+  write-host -Backgroundcolor Green -Foregroundcolor Black "Web: " $web.Url "`r`n"
   # If you want to automatically activate languages on all Sites
-  #$web.IsMultilingual = $true;
-  #write-host -Backgroundcolor Green -Foregroundcolor Black "Multilanguage allowed:" $web.IsMultilingual
+  $web.IsMultilingual = $true;
+  write-host -Backgroundcolor Green -Foregroundcolor Black "Multilanguage allowed:" $web.IsMultilingual "`r`n"
+
+  #Get installed languages
+  $RegionSettings = New-Object Microsoft.SharePoint.SPRegionalSettings($web)
+  $regLangs = $RegionSettings.InstalledLanguages
   # Check if Multilanguage Support is activated
   # If so, activate all the other available languages
   if($web.IsMultilingual -eq $true)
   {
-    foreach($culture in $web.SupportedUICultures)
+    foreach($regLang in $regLangs)
     {
-      $web.AddSupportedUICulture($culture.LCID)
-      write-host -Backgroundcolor Yellow -Foregroundcolor Black "Language " $culture.Name " added."
-      $web.Update()
+      $LCID = $regLang.LCID
+      $web.AddSupportedUICulture($LCID)
+      write-host -Backgroundcolor Yellow -Foregroundcolor Black "Language " $regLang.DisplayName  " / " $LCID " added." "`r`n"
     }
   }
+  $web.Update()
+  "`r`n"
   $web.Dispose()
 }
-write-host -Backgroundcolor White -Foregroundcolor Black "Done."
-read-host "Press ENTER to quit..."
+write-host -Backgroundcolor White -Foregroundcolor Black "Done." "`r`n"
+read-host "Press ENTER to quit..." "`r`n"
+stop-transcript
--- ActivateSelectedAlternateLanguages.ps1
+++ ActivateSelectedAlternateLanguages.ps1
@@ -3,7 +3,7 @@
 # twitter: twitter.com/pko3
 # github: github.com/pkothree
 # Created: 07/14/15
-# Modified: 07/14/15
+# Modified: 07/15/15
 # Description: Activate selected alternate languages on all site collections
 ###
 
@@ -13,32 +13,40 @@ Add-PSSnapin Microsoft.SharePoint.PowerShell
 }
 
 $webApp = Get-SPWebApplication WEBAPP
-$languages = ("English","French","German")
+$languages = ("English","French","German", "Spanish")
+$w = $WebApp.Name
+
+start-transcript -path $w".txt"
 
 foreach($site in $webApp.Sites)
 {
   $web = $site.Rootweb
-  write-host -Backgroundcolor Green -Foregroundcolor Black "Web: " $web
+  write-host -Backgroundcolor Green -Foregroundcolor Black "Web: " $web "`r`n"
   $RegionSettings = New-Object Microsoft.SharePoint.SPRegionalSettings($web)
   foreach($language in $languages)
   {
-    if($language -eq $RegionSettings.InstalledLanguages.DisplayName)
+  $regLangs = $RegionSettings.InstalledLanguages
+  foreach($regLang in $regLangs)
+  {
+    if($language -eq $regLang.DisplayName)
     {
-      $LCID = $RegionSettings.InstalledLanguages.LCID
+      $LCID = $regLang.LCID
       # If you want to automatically activate languages on all Sites
-      #$web.IsMultilingual = $true;
-      #write-host -Backgroundcolor Green -Foregroundcolor Black "Multilanguage allowed:" $web.IsMultilingual
+      $web.IsMultilingual = $true;
+      write-host -Backgroundcolor Green -Foregroundcolor Black "Multilanguage allowed:" $web.IsMultilingual "`r`n"
       # Check if Multilanguage Support is activated
       # If so, activate all the other available languages
       if($web.IsMultilingual -eq $true)
       {
         $web.AddSupportedUICulture($LCID)
-        write-host -Backgroundcolor Yellow -Foregroundcolor Black  "Language " $language " added."
+        write-host -Backgroundcolor Yellow -Foregroundcolor Black  "Language " $language  " / " $LCID " added." "`r`n"
         $web.Update()
       }
     }
   }
+  }
   $web.Dispose()
 }
-write-host -Backgroundcolor White -Foregroundcolor Black "Done."
-read-host "Press ENTER to quit..."
+write-host -Backgroundcolor White -Foregroundcolor Black "Done." "`r`n"
+read-host "Press ENTER to quit..." "`r`n"
+stop-transcript
